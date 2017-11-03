c088e431b3bdb35bb0e44041c04f0253879a19ec
Natalie Jerabek <wi16b023@technikum-wien.at>
2017-11-03 21:16:13 +0100

    add complete Coding Dojo 1

---
+++ CodingDojo_01/Stack.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingDojo_01
+{
+    class Stack<T>
+    {
+
+        private StackElement<T> current;
+
+        //successor muss direkt im StackElement gespeichert werden! sonst keine LinkedList
+        //private StackElement<T> successor;
+
+        //push-Methode: neues Objekt hinzufügen 
+        public void Push(T item)
+        {
+            if (current == null)
+            {
+                current = new StackElement<T>() { Value = item, PreSuccessor = null };
+            }
+            else
+            {
+                StackElement<T> temp = new StackElement<T>() { Value = item, PreSuccessor = current };
+                current = temp;
+            }
+        }
+
+        //pop-Methode: ein Objekt entfernen + gelöschtes soll wiedergegeben werden
+        public T Pop()
+        {
+            if (current != null)
+            {
+                T temp = current.Value;
+                current = current.PreSuccessor;
+                return temp;
+            }
+            else
+            {
+                throw new NullReferenceException();
+            }
+        }
+
+        //peek-Methode: gibt das zuletzt hinzugefügte Objekt aus
+        public T Peek()
+        {
+            if (current != null)
+            {
+                return current.Value;
+            }
+            else
+            {
+                return default(T);
+            }
+        }
+
+    }
+}
---
+++ CodingDojo_01/StackElement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingDojo_01
+{
+    class StackElement<T>
+    {
+        public T Value { get; set; } //speichert den Wert des neuen Objektess
+
+        public StackElement<T> PreSuccessor { get; set; }
+    }
+}
